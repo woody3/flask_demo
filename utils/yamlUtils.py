193fef2d91463a6a435b124a0ed6fd5a4ce382ca
woody3 <35905144+woody3@users.noreply.github.com>
2023-09-01 20:39:22 +0800

    Update yamlUtils.py

--- utils/yamlUtils.py
+++ utils/yamlUtils.py
@@ -1,12 +1,22 @@
-import os.path
+import os
 import yaml
 
 
-def parse_yaml(file_name):
-    params = {}
-    base_path = os.path.dirname(os.path.dirname(__file__))
-    yaml_path = os.path.join(base_path, "config", file_name)
-    with open(yaml_path, "r", encoding="utf-8") as file:
-        params = yaml.load(file.read(), Loader=yaml.SafeLoader)
-    return params
+class Config(object):
+    @classmethod
+    def parse(cls, file):
+        config_file = os.path.join(os.path.dirname(os.getcwd(), "config", file))
+        with open(config_file, mode="r", encoding="utf-8") as f:
+            return yaml.load(f.read(), Loader=yaml.FullLoader)
 
+    @classmethod
+    def get_wx_path(cls):
+        return cls.parse("maotai.yaml").get("wx_path")
+
+    @classmethod
+    def boot_waiting_sec(cls):
+        return cls.parse("maotai.yaml").get("boot_waiting_sec")
+
+    @classmethod
+    def qrcode_refresh_sec(cls):
+        return cls.parse("maotai.yaml").get("qrcode_refresh_sec")
